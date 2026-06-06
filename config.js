48067f378e9ec1e8f97e455b62b7da4b95c79503
neerajvavi <274112777+neerajvavi@users.noreply.github.com>
2026-06-06 10:59:11 +0530

    Configure Supabase

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 export const SUPABASE_CONFIG = {
-  url: "",
-  anonKey: ""
+  url: "https://hrrmpueqwasvujewfucz.supabase.co/rest/v1/",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhycm1wdWVxd2FzdnVqZXdmdWN6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA1NTYyODYsImV4cCI6MjA5NjEzMjI4Nn0.QFsfGdAqiQ7b6K5qJyNQNRXpNp9sRy6Zqlk3QqAjmc0"
 };
